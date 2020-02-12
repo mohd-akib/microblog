4b14c5a55442cb301cca9c369d8bcbf29fa8c19c
Mohd Akib <mohd.akib@medlife.com>
2020-02-13 00:31:26 +0530

    added simple web form

--- hello.py
+++ hello.py
@@ -1,9 +1,49 @@
-from flask import Flask 
-app = Flask(__name__)
+from flask import Flask, render_template
+from flask import request
+from flask_bootstrap import Bootstrap
+from datetime import datetime
+from flask_moment import Moment
+from flask_wtf import Form
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+from flask import Flask, render_template, sessions, url_for, redirect
 
-@app.route('/')
+app = Flask(__name__)
+bootstrap = Bootstrap(app)
+moment = Moment(app)
+app.config['SECRET_KEY'] = 'hard to guess string'
+
+
+class NameForm(Form):
+    name = StringField('What is your name?', validators=[DataRequired()])
+    submit = SubmitField('Submit')
+
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
-	return '<h1>hello World!</h1>'
+    name = None
+    form = NameForm()
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ''
+    return render_template('index.html', current_time=datetime.utcnow(), form=form, name=name)
+
+
+@app.route('/user/<name>')
+def user(name):
+    header = request.headers
+    return render_template('user.html', name=name)
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
+
 
 if __name__ == "__main__":
-	app.run(debug = True)
+    app.run(debug = True)
