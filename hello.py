aa6ba40a4690fd9e17befe6ca079325e23726a38
Mohd Akib <mohd.akib@medlife.com>
2020-02-13 00:57:10 +0530

    added flash functionaly of flask in form

--- hello.py
+++ hello.py
@@ -6,7 +6,7 @@ from flask_moment import Moment
 from flask_wtf import Form
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
-from flask import Flask, render_template, sessions, url_for, redirect
+from flask import Flask, render_template, session, url_for, redirect, flash
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
@@ -21,12 +21,15 @@ class NameForm(Form):
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    name = None
     form = NameForm()
     if form.validate_on_submit():
-        name = form.name.data
+        old_name = session.get('name')
+        if old_name is not None and old_name != form.name.data:
+            flash("looks like you've changed your name")
+        session['name'] = form.name.data
         form.name.data = ''
-    return render_template('index.html', current_time=datetime.utcnow(), form=form, name=name)
+        return redirect(url_for('index'))
+    return render_template('index.html', current_time=datetime.utcnow(), form=form, name=session.get('name'))
 
 
 @app.route('/user/<name>')
